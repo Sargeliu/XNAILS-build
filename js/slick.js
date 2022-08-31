1bbc4066ba8c34dc9b517625345b4c257a7e80d0
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2022-08-31 21:35:19 +0500

    add

--- js/slick.js
+++ js/slick.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){$(".carousel__list").slick({dots:!0,autoplaySpeed:2e3,arrows:!1,adaptiveHeight:!0})}));
+$(document).ready((function(){$(".carousel__list").slick({dots:!0,autoplay:!0,autoplaySpeed:2e3,arrows:!1,adaptiveHeight:!0}),$("ul.tabs__caption").on("click","li:not(.tabs__item--active)",(function(){$(this).addClass("tabs__item--active").siblings().removeClass("tabs__item--active").closest("div.tabs").find("div.tabs__content").removeClass("tabs__content--active").eq($(this).index()).addClass("tabs__content--active")}))}));
